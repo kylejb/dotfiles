2ed80ce35cc074a74367740064236a8fcce88ce4
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2024-01-15 07:55:56 -0500

    fix(vscode): update extensions

--- vscode/update.sh
+++ vscode/update.sh
@@ -3,17 +3,16 @@
 update_extensions() {
     echo "Updating VSCode extensions..."
 
-    code --install-extension "aaron-bond.better-comments"
-    code --install-extension "christian-kohler.path-intellisense"
-    code --install-extension "dbaeumer.vscode-eslint"
-    code --install-extension "eamodio.gitlens"
-    code --install-extension "esbenp.prettier-vscode"
-    code --install-extension "golang.go"
-    code --install-extension "gruntfuggly.todo-tree"
-    code --install-extension "jasonlhy.hungry-delete"
-    code --install-extension "mhutchie.git-graph"
-    code --install-extension "ms-vscode-remote.remote-containers"
-    code --install-extension "prisma.prisma"
+    code --force --install-extension "aaron-bond.better-comments"
+    code --force --install-extension "christian-kohler.path-intellisense"
+    code --force --install-extension "dbaeumer.vscode-eslint"
+    code --force --install-extension "eamodio.gitlens"
+    code --force --install-extension "esbenp.prettier-vscode"
+    code --force --install-extension "golang.go"
+    code --force --install-extension "gruntfuggly.todo-tree"
+    code --force --install-extension "jasonlhy.hungry-delete"
+    code --force --install-extension "mhutchie.git-graph"
+    code --force --install-extension "ms-vscode-remote.remote-containers"
 }
 
 update_extensions

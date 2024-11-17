807777196fd7237539d5af32ad605b962d401af3
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2024-11-17 09:41:33 -0500

    chore(mise): update tool dependencies

--- mise/update.sh
+++ mise/update.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-mise self-update
+mise self-update -y
 mise up
+
+# update Ruby gems
+gem update --system
+gem update
+gem cleanup
+
+# update Node.js package manager
+corepack enable
+npm i -g npm@latest
+
+# update global Python packages
+command -v pipx && pipx reinstall-all
